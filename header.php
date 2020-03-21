354f81ef9c392c8923f82d10e57dae0637cda6cb
Luis <loic.quiros@gmail.com>
2020-03-20 22:13:02 -0700

    d20: href simplified

--- header.php
+++ header.php
@@ -7,19 +7,19 @@
 <body>
 	<div class="head-container">
 		<div class="logo">
-			<a href="http://e1z3r1p12.42.fr:8080/Vog_Wildcard/home.php">
+			<a href="home.php">
 				<img  src="logo_1.png" height="70" alt="Panic.bear">
 			</a>
 		</div>
 		<div class="navi">
-			<a id="add" href="http://e1z3r1p12.42.fr:8080/Vog_Wildcard/add.php">ADD</a>
-			<a id="history" href="http://e1z3r1p12.42.fr:8080/Vog_Wildcard/history.php">HISTORY</a>
-			<a id="analytics" href="http://e1z3r1p12.42.fr:8080/Vog_Wildcard/analytics.php">ANALYTICS</a>
+			<a id="add" href="add.php">ADD</a>
+			<a id="history" href="history.php">HISTORY</a>
+			<a id="analytics" href="analytics.php">ANALYTICS</a>
 			<div class="dropdown">
 				<img class="dropbtn" src="cashew_icon.svg" height="20">
 				<div class="dropdown-content">
-					<a id="login" href="http://e1z3r1p12.42.fr:8080/Vog_Wildcard/login.php">LOGIN</a>
-					<a id="signup" href="http://e1z3r1p12.42.fr:8080/Vog_Wildcard/signup.php">SIGN UP</a>
+					<a id="login" href="login.php">LOGIN</a>
+					<a id="signup" href="signup.php">SIGN UP</a>
 				</div>
 			</div>
 		</div>
