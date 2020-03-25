1ec9a645ce222d4aff1f786c410860a47ecd89cd
Luis <loic.quiros@gmail.com>
2020-03-24 21:31:05 -0700

    signup data insertion

--- signup.php
+++ signup.php
@@ -9,15 +9,25 @@ include("header.php");
 	<link rel="stylesheet" type="text/css" href="login_signup.css">
 </head>
 
+<?php
+
+if ($_POST["username"] && $_POST["password"] && $_POST["submit"] == "SIGN UP")
+{
+	$conn = mysqli_connect("localhost", "luis", "", "db_climb");	
+	mysqli_query($conn, "INSERT INTO tb_users (username, password) VALUES ('$_POST[username]', '$_POST[password]')");
+	mysqli_close($conn);
+}
+
+?>
 
 <body>
 <div class="signup-container">
 	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" id="add" method="post">
 	<div class="signup-inside">
 		<div><input type="text" id="username" name="username" placeholder="User Name"></div>
-		<div><input type="password" id="pass" name="pass" placeholder="Password"></div>
+		<div><input type="password" id="password" name="password" placeholder="Password"></div>
 		<br>
-		<div><input type="submit" id="submit" value="SIGN UP"></div>
+		<div><input type="submit" id="submit" name ="submit" value="SIGN UP"></div>
 	</div>
 	</form>
 </div>
