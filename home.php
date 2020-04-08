867316fed0a06695c05710835597575333ae93c7
Luis <loic.quiros@gmail.com>
2020-04-07 21:32:53 -0700

    d35: trying to put the footer in the bottom

--- home.php
+++ home.php
@@ -2,9 +2,17 @@
 <head>
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="home.css">
+	<script src="jquery.js"></script>
+	<script>
+		$(function() {
+			$("#incl").load("footer.php");
+		});
+	</script>
+	
 </head>
 
 <body>
+<div class="all">
 <?php
 if (isset($_COOKIE["user"]))
 	include("header_in.php");
@@ -16,8 +24,10 @@ else
 			<img id="pic" src="betamonkeys.jpeg" alt="betamonkeys">
 		</div>
 	</div>
+	<div id="incl"></div>
 <?php
 include("footer.php");
 ?>
+</div>
 </body>
 </html>
