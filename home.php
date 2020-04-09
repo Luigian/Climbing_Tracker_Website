38b0574c8240e4b8bbe1ced6b37c27e7c81dc821
Luis <loic.quiros@gmail.com>
2020-04-08 21:21:49 -0700

    d36: second try to put footer at the bottom

--- home.php
+++ home.php
@@ -2,32 +2,22 @@
 <head>
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="home.css">
-	<script src="jquery.js"></script>
-	<script>
-		$(function() {
-			$("#incl").load("footer.php");
-		});
-	</script>
-	
 </head>
 
 <body>
-<div class="all">
 <?php
 if (isset($_COOKIE["user"]))
-	include("header_in.php");
+	include "header_in.php";
 else
-	include("header_out.php");
+	include "header_out.php";
 ?>
 	<div class="home-container">
 		<div class="photo">
 			<img id="pic" src="betamonkeys.jpeg" alt="betamonkeys">
 		</div>
 	</div>
-	<div id="incl"></div>
 <?php
-include("footer.php");
+include "footer.php";
 ?>
-</div>
 </body>
 </html>
