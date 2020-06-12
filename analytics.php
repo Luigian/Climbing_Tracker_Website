61df4c7196f69a8604911c590cd21ed22698a0f3
Luis <loic.quiros@gmail.com>
2020-06-11 22:25:53 -0700

    d73: Fix the queries, identation and warnings on the 'analytics' page

--- analytics.php
+++ analytics.php
@@ -1,5 +1,9 @@
 <?php
-include("header_in.php");
+	include("header_in.php");
+
+	$conn = mysqli_connect("localhost", "luis", "", "db_climb");
+	$q_count = mysqli_query($conn, "SELECT COUNT(*) FROM climbs WHERE userId = '$_COOKIE[userId]'");
+	$row_count = mysqli_fetch_array($q_count);
 ?>
 
 <html>
@@ -10,103 +14,98 @@ include("header_in.php");
 
 <body>
 	<?php
-	$conn = mysqli_connect("localhost", "luis", "", "db_climb");
-	$user = "tb_".$_COOKIE["user"];
-	$q_count = mysqli_query($conn, "SELECT COUNT(*) FROM $user");
-	$row_count = mysqli_fetch_array($q_count);
-	if ($row_count[0] == '0')
-	{
-		echo "<div class='msg-container'>";
-		echo "<a id='addmessage' href='add.php'>Add your first climb here</a>";
-		echo "</div>";
-	}
-	else
-	{
-		?>	
-		<div class="table-container">
-		<table>
-		<tr>
-			<th>Grade</th>
-			<th>Efficacy</th>
-			<th>Efficiency</th>
-			<th>Climbs</th>
-			<th>Distinct Climbs</th>
-			<th>Tops</th>
-			<th>Distinct Tops</th>
-		</tr>
-		<?php
-		$gr = array("5.8", "5.9", "5.10a", "5.10b", "5.10c", "5.10d", "5.11a", "5.11b", "5.11c", 
-		"5.11d", "5.12a", "5.12b", "5.12c", "5.12d");
-		$i = 0;
-		while ($gr[$i])
+		if ($row_count[0] == '0')
 		{
-			$x = $gr[$i];
-			$q_c = mysqli_query($conn, "SELECT COUNT(*) FROM $user WHERE grade = '$x'");
-			$q_t = mysqli_query($conn, "SELECT COUNT(*) FROM $user WHERE grade = '$x'
-			   	AND status = 'Top'");
-			$q_dc = mysqli_query($conn, "SELECT COUNT(DISTINCT route_id) FROM $user
-			   	WHERE grade = '$x'");
-			$q_dt = mysqli_query($conn, "SELECT COUNT(DISTINCT route_id) FROM $user
-			   	WHERE grade = '$x' AND status = 'Top'");
+			echo
+			"<div class='msg-container'>
+				<a id='addmessage' href='add.php'>Add your first climb here</a>
+			</div>";
+		}
+		else
+		{
+			echo
+			'<div class="table-container">
+				<table>
+					<tr>
+						<th>Grade</th>
+						<th>Efficacy</th>
+						<th>Efficiency</th>
+						<th>Climbs</th>
+						<th>Distinct Climbs</th>
+						<th>Tops</th>
+						<th>Distinct Tops</th>
+					</tr>';
+		
+			$gr = array("5.8", "5.9", "5.10a", "5.10b", "5.10c", "5.10d", "5.11a", "5.11b", "5.11c", "5.11d", "5.12a", "5.12b", "5.12c", "5.12d");
+			$i = 0;
+			while ($i < 14)
+			{
+				$x = $gr[$i];
+				$q_c = mysqli_query($conn, "SELECT COUNT(*) FROM climbs LEFT JOIN routes ON routes.id = climbs.routeId WHERE climbs.userId = '$_COOKIE[userId]' AND routes.grade = '$x'");
+				$q_t = mysqli_query($conn, "SELECT COUNT(*) FROM climbs LEFT JOIN routes ON routes.id = climbs.routeId WHERE climbs.userId = '$_COOKIE[userId]' AND routes.grade = '$x' AND climbs.status = 'Top'");
+				$q_dc = mysqli_query($conn, "SELECT COUNT(DISTINCT climbs.routeId) FROM climbs LEFT JOIN routes ON routes.id = climbs.routeId WHERE climbs.userId = '$_COOKIE[userId]' AND routes.grade = '$x'");
+				$q_dt = mysqli_query($conn, "SELECT COUNT(DISTINCT climbs.routeId) FROM climbs LEFT JOIN routes ON routes.id = climbs.routeId WHERE climbs.userId = '$_COOKIE[userId]' AND routes.grade = '$x' AND climbs.status = 'Top'");
+				$row_c = mysqli_fetch_array($q_c);
+				$row_t = mysqli_fetch_array($q_t);
+				$row_dc = mysqli_fetch_array($q_dc);
+				$row_dt = mysqli_fetch_array($q_dt);
+				if ($row_c[0] != 0)
+				{	
+					$efc = ($row_dt[0] / $row_dc[0]) * 100;
+					$efc = (int)$efc;
+					$efn = ($row_t[0] / $row_c[0]) * 100;
+					$efn = (int)$efn;
+					$clm = $row_c[0];
+					$dclm = $row_dc[0];
+					$top = $row_t[0];
+					$dtop = $row_dt[0];
+					echo
+					'<tr>
+						<td class="analytics-row">'.$x.'</td>
+						<td>'.$efc.'%</td>
+						<td>'.$efn.'%</td>
+						<td>'.$clm.'</td>
+						<td>'.$dclm.'</td>
+						<td>'.$top.'</td>
+						<td>'.$dtop.'</td>
+					</tr>';
+				}
+				$i = $i + 1;
+			}
+			$q_c = mysqli_query($conn, "SELECT COUNT(*) FROM climbs WHERE userId = '$_COOKIE[userId]'");
+			$q_t = mysqli_query($conn, "SELECT COUNT(*) FROM climbs WHERE userId = '$_COOKIE[userId]' AND status = 'Top'");
+			$q_dc = mysqli_query($conn, "SELECT COUNT(DISTINCT routeId) FROM climbs WHERE userId = '$_COOKIE[userId]'");
+			$q_dt = mysqli_query($conn, "SELECT COUNT(DISTINCT routeId) FROM climbs WHERE userId = '$_COOKIE[userId]' AND status = 'Top'");
 			$row_c = mysqli_fetch_array($q_c);
 			$row_t = mysqli_fetch_array($q_t);
 			$row_dc = mysqli_fetch_array($q_dc);
 			$row_dt = mysqli_fetch_array($q_dt);
-			if ($row_c[0] != 0)
-			{	
-				$efc = ($row_dt[0] / $row_dc[0]) * 100;
-				$efc = (int)$efc;
-				$efn = ($row_t[0] / $row_c[0]) * 100;
-				$efn = (int)$efn;
-				$clm = $row_c[0];
-				$dclm = $row_dc[0];
-				$top = $row_t[0];
-				$dtop = $row_dt[0];
-				echo	'<tr>';
-				echo	'<td class="analytics-row">'.$x.'</td>';
-				echo	'<td>'.$efc.'%</td>';
-				echo	'<td>'.$efn.'%</td>';
-				echo	'<td>'.$clm.'</td>';
-				echo	'<td>'.$dclm.'</td>';
-				echo	'<td>'.$top.'</td>';
-				echo	'<td>'.$dtop.'</td>';
-				echo	'</tr>';
-			}
-			$i = $i + 1;
+			$efc = ($row_dt[0] / $row_dc[0]) * 100;
+			$efc = (int)$efc;
+			$efn = ($row_t[0] / $row_c[0]) * 100;
+			$efn = (int)$efn;
+			$clm = $row_c[0];
+			$dclm = $row_dc[0];
+			$top = $row_t[0];
+			$dtop = $row_dt[0];
+					echo
+					'<tr>
+						<th>Totals</th>
+						<th>'.$efc.'%</th>
+						<th>'.$efn.'%</th>
+						<th>'.$clm.'</th>
+						<th>'.$dclm.'</th>
+						<th>'.$top.'</th>
+						<th>'.$dtop.'</th>
+					</tr>
+				</table>
+			</div>';
 		}
-		$q_c = mysqli_query($conn, "SELECT COUNT(*) FROM $user");
-		$q_t = mysqli_query($conn, "SELECT COUNT(*) FROM $user WHERE status = 'Top'");
-		$q_dc = mysqli_query($conn, "SELECT COUNT(DISTINCT route_id) FROM $user");
-		$q_dt = mysqli_query($conn, "SELECT COUNT(DISTINCT route_id) FROM $user WHERE status = 'Top'");
-		$row_c = mysqli_fetch_array($q_c);
-		$row_t = mysqli_fetch_array($q_t);
-		$row_dc = mysqli_fetch_array($q_dc);
-		$row_dt = mysqli_fetch_array($q_dt);
-		$efc = ($row_dt[0] / $row_dc[0]) * 100;
-		$efc = (int)$efc;
-		$efn = ($row_t[0] / $row_c[0]) * 100;
-		$efn = (int)$efn;
-		$clm = $row_c[0];
-		$dclm = $row_dc[0];
-		$top = $row_t[0];
-		$dtop = $row_dt[0];
-		echo	'<tr>';
-		echo	'<th>Totals</th>';
-		echo	'<th>'.$efc.'%</th>';
-		echo	'<th>'.$efn.'%</th>';
-		echo	'<th>'.$clm.'</th>';
-		echo	'<th>'.$dclm.'</th>';
-		echo	'<th>'.$top.'</th>';
-		echo	'<th>'.$dtop.'</th>';
-		echo	'</tr>';
-		echo '</table>';
-		echo '</div>';
-	}
-	mysqli_close($conn);
+		mysqli_close($conn);
 	?>
 </body>
 </html>
 
 <?php
-include("footer.php");
+	include("footer.php");
 ?>
