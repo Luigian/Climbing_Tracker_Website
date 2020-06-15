fc90f387ad422eacde4b3273fa45c7068c417ff3
Luis <loic.quiros@gmail.com>
2020-06-14 21:09:01 -0700

    d75: Update the display and style of 'dashboard' page, move the totals row to the top, change color to fonts, delete some texts

--- dashboard.php
+++ dashboard.php
@@ -23,19 +23,6 @@
 		}
 		else
 		{
-			echo
-			'<div class="dashboard-container">
-				<table>
-					<tr>
-						<th>Grade</th>
-						<th>Setted</th>
-						<th>%</th>
-						<th>Actives</th>
-						<th>%</th>
-						<th>Climbs</th>
-						<th>%</th>
-					</tr>';
-		
 			$q_st = mysqli_query($conn, "SELECT COUNT(*) FROM routes WHERE gymId = '$_COOKIE[gymAdmId]'");
 			$q_at = mysqli_query($conn, "SELECT COUNT(*) FROM routes WHERE gymId = '$_COOKIE[gymAdmId]' AND active = '1'");
 			$q_ct = mysqli_query($conn, "SELECT COUNT(*) FROM climbs LEFT JOIN routes ON routes.id = climbs.routeId WHERE routes.gymId = '$_COOKIE[gymAdmId]'");
@@ -43,8 +30,30 @@
 			$row_at = mysqli_fetch_array($q_at);
 			$row_ct = mysqli_fetch_array($q_ct);
 			$setted_totals = $row_st[0];
-			$actives_totals = $row_at[0];
+			$active_totals = $row_at[0];
 			$climbs_totals = $row_ct[0];
+			
+			echo
+			'<div class="dashboard-container">
+				<table>
+					<tr>
+						<th>Grade</th>
+						<th>Setted</th>
+						<th></th>
+						<th>Active</th>
+						<th></th>
+						<th>Climbs</th>
+						<th></th>
+					</tr>
+					<tr id="dash-totals">
+						<th>All</th>
+						<th>'.$setted_totals.'</th>
+						<th></th>
+						<th>'.$active_totals.'</th>
+						<th></th>
+						<th>'.$climbs_totals.'</th>
+						<th></th>
+					</tr>';
 			
 			$gr = array("5.8", "5.9", "5.10a", "5.10b", "5.10c", "5.10d", "5.11a", "5.11b", "5.11c", "5.11d", "5.12a", "5.12b", "5.12c", "5.12d");
 			$i = 0;
@@ -66,11 +75,11 @@
 						$setted_percent += .5;
 					$setted_percent = (int)$setted_percent;
 
-					$actives = $row_a[0];
-					$actives_percent = ($actives / $actives_totals) * 100;
-					if ($actives_percent - (int)$actives_percent > .49)
-						$actives_percent += .5;
-					$actives_percent = (int)$actives_percent;
+					$active = $row_a[0];
+					$active_percent = ($active / $active_totals) * 100;
+					if ($active_percent - (int)$active_percent > .49)
+						$active_percent += .5;
+					$active_percent = (int)$active_percent;
 
 					$climbs = $row_c[0];
 					$climbs_percent = ($climbs / $climbs_totals) * 100;
@@ -79,29 +88,20 @@
 					$climbs_percent = (int)$climbs_percent;
 
 					echo
-					'<tr>
-						<td class="dashboard-row">'.$x.'</td>
+					'<tr">
+						<th>'.$x.'</th>
 						<td>'.$setted.'</td>
-						<td>'.$setted_percent.'%</td>
-						<td>'.$actives.'</td>
-						<td>'.$actives_percent.'%</td>
+						<td class="percent">'.$setted_percent.'%</td>
+						<td>'.$active.'</td>
+						<td class="percent">'.$active_percent.'%</td>
 						<td>'.$climbs.'</td>
-						<td>'.$climbs_percent.'%</td>
+						<td class="percent">'.$climbs_percent.'%</td>
 					</tr>';
 				}
 				$i = $i + 1;
 			}
-					echo
-					'<tr>
-						<th>Totals</th>
-						<th>'.$setted_totals.'</th>
-						<th>100%</th>
-						<th>'.$actives_totals.'</th>
-						<th>100%</th>
-						<th>'.$climbs_totals.'</th>
-						<th>100%</th>
-					</tr>
-				</table>
+			echo
+				'</table>
 			</div>';
 		}
 		mysqli_close($conn);
